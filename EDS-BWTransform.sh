20398f06edab9edab978b96ef8c115cb10d2d34f
Veronica Guerrini <44610993+veronicaguerrini@users.noreply.github.com>
2025-02-17 13:11:37 +0100

    Update EDS-BWTransform.sh

--- EDS-BWTransform.sh
+++ EDS-BWTransform.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-#eds_to_ebwt.sh NAMEFILE PATHTOOL
+BCR=1
 
+BCRPATH="BCR_LCP_GSA"
 GSUFPATH="gsufsort"
+
 NAMEFILE=$1
 OUTPUT=$2
 
 ./eds_to_fasta $NAMEFILE.eds $OUTPUT
 
-$GSUFPATH/gsufsort $OUTPUT.fasta --da --bwt --output $OUTPUT
-rm $OUTPUT.fasta 
-
-./da_to_everything $OUTPUT
+if [ $BCR -eq 1 ]
+then
+    $BCRPATH/"BCR_LCP_GSA" $OUTPUT.fasta $OUTPUT
+	rm $OUTPUT.fasta
+	rm $OUTPUT.len
+	rm $OUTPUT.info
+	./EOFpos_to_everything $OUTPUT
+else
+    $GSUFPATH/"gsufsort" $OUTPUT.fasta --da --bwt --output $OUTPUT
+	rm $OUTPUT.fasta
+	./da_to_everything $OUTPUT
+fi 
 
 echo "File "$NAMEFILE" done."
