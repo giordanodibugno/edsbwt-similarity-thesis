a5acf3b82af2f9413e71dcdb3568306f4a13942a
Veronica Guerrini <44610993+veronicaguerrini@users.noreply.github.com>
2026-02-10 15:26:05 +0100

    Bug fixed

--- eds_to_fasta.cpp
+++ eds_to_fasta.cpp
@@ -19,7 +19,7 @@ int main (int argc, char *argv[]){
 	char null_char_input = EMPTY_CHAR_EDS;
 	char null_char=EMPTY_CHAR;	//must be lexicographically greater than every other letter in the input file.
 	bit_vector b;
-	int empty_counter=0;
+	dataTypeNChar empty_counter=0;
 
 
 	string inputName = argv[1];
